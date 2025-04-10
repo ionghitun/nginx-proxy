cad5bcaa4d94ea87ed7a82a7faf79e5ee645ddb2
Ion Ghițun <ionghitun@gmail.com>
2025-04-10 17:12:15 +0300

    small fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,8 @@ echo "Want to update images before rebuild? (y/n) [default: y]: "
 read UPDATE_IMAGES
 UPDATE_IMAGES=${UPDATE_IMAGES:-y}
 
+COMPOSE_PROFILES=$(grep -oP '^COMPOSE_PROFILES=\K.*' .env)
+
 # Check user input in a POSIX-compatible way
 if [ "$UPDATE_IMAGES" = "y" ] || [ "$UPDATE_IMAGES" = "Y" ]; then
     NGINX_PROXY_VERSION=$(grep -oP '^NGINX_PROXY_VERSION=\K.*' .env)
@@ -15,9 +17,13 @@ if [ "$UPDATE_IMAGES" = "y" ] || [ "$UPDATE_IMAGES" = "Y" ]; then
     SELF_SIGNED_VERSION=$(grep -oP '^SELF_SIGNED_VERSION=\K.*' .env)
 
     docker pull "nginxproxy/nginx-proxy:$NGINX_PROXY_VERSION"
-    docker pull "nginxproxy/acme-companion:$ACME_COMPANION_VERSION"
-    docker pull "sebastienheyd/self-signed-proxy-companion:$SELF_SIGNED_VERSION"
-    docker pull alpine:latest
+
+    if [ "$COMPOSE_PROFILES" = "acme" ]; then
+        docker pull "nginxproxy/acme-companion:$ACME_COMPANION_VERSION"
+    elif [ "$COMPOSE_PROFILES" = "self-signed" ]; then
+        docker pull "sebastienheyd/self-signed-proxy-companion:$SELF_SIGNED_VERSION"
+        docker pull alpine:latest
+    fi
 fi
 
 echo "*** Rebuilding application ***"
