ec11b97cfded04bb62583ec349bba6855d2820a1
Ion Ghitun <ionghitun@gmail.com>
2025-11-16 15:03:42 +0200

    master - few fixes and improvements

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,16 +6,16 @@ printf "Do you want to update images before rebuilding? (y/n) [default: y]: "
 read UPDATE_IMAGES
 UPDATE_IMAGES=${UPDATE_IMAGES:-y}
 
-COMPOSE_PROFILES=$(sed -n 's/^COMPOSE_PROFILES=//p' .env)
+COMPOSE_PROFILES=$(sed -n 's/^COMPOSE_PROFILES=//p' .env | tr -d '\r' | tr -d '"')
 
 if [ "$UPDATE_IMAGES" = "y" ] || [ "$UPDATE_IMAGES" = "Y" ]; then
     echo
     echo "===== Updating images... ====="
     echo
 
-    NGINX_PROXY_VERSION=$(sed -n 's/^NGINX_PROXY_VERSION=//p' .env)
-    ACME_COMPANION_VERSION=$(sed -n 's/^ACME_COMPANION_VERSION=//p' .env)
-    SELF_SIGNED_VERSION=$(sed -n 's/^SELF_SIGNED_VERSION=//p' .env)
+    NGINX_PROXY_VERSION=$(sed -n 's/^NGINX_PROXY_VERSION=//p' .env | tr -d '\r' | tr -d '"')
+    ACME_COMPANION_VERSION=$(sed -n 's/^ACME_COMPANION_VERSION=//p' .env | tr -d '\r' | tr -d '"')
+    SELF_SIGNED_VERSION=$(sed -n 's/^SELF_SIGNED_VERSION=//p' .env | tr -d '\r' | tr -d '"')
 
     docker pull "nginxproxy/nginx-proxy:$NGINX_PROXY_VERSION"
 
